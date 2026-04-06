1525523532232bc7828f7a345997e587b09aa0ff
Mehul Kumar <mehulsubhash888@gmail.com>
2026-04-06 11:26:26 +0530

    Implement play again functionality in guessing game
    Added a play again feature to the number guessing game.

--- Source_Code.c
+++ Source_Code.c
@@ -7,29 +7,34 @@
 
 int main()
 {
-    srand(time(0));
-    int RandomNumber = (rand() % 100) + 1;
-    int x;
-    int count = 0;
+    int play_again;
 
-    do
-    {
-        printf("Guess The Number:");
-        scanf("%d", &x);
-        if (x < RandomNumber){
-            printf("Higher Number Please\n");
-        }
-        else if (x > RandomNumber){
-            printf("Lower Number Please\n+");
-        }
-        else{
-            printf("Congrats");
-        }
-        count++;
-    } while (x != RandomNumber);
+    do{
+        srand(time(0));
+        int RandomNumber = (rand() % 100) + 1;
+        int x;
+        int count = 0;
 
+        do
+        {
+            printf("Guess The Number:");
+            scanf("%d", &x);
+            if (x < RandomNumber){
+                printf("Higher Number Please\n");
+            }
+            else if (x > RandomNumber){
+                printf("Lower Number Please\n+");
+            }
+            else{
+                printf("Congrats");
+            }
+            count++;
+        } while (x != RandomNumber);
     printf("Great You Guessed It Right\n The Number Was %d", RandomNumber);
     printf("\nYou Figured it in attempt %d", count + 1);
+    printf("\nWould you like to try again, submit 1 for yess and 0 to quite:");
+    scanf("%d",&play_again);
+    }while(play_again == 1) ;
 
     return 0;
 }

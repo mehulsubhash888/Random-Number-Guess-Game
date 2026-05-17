fa6ca57c2f5276fcb5896764e6accddc3e541d8d
Mehul Kumar <mehulsubhash888@gmail.com>
2026-05-17 19:55:11 +0530

    Remove redundant code and clean up formatting

--- Source_Code.c
+++ Source_Code.c
@@ -8,7 +8,7 @@
 int main()
 {
     printf("Welcome\nIn this game, the computer will think of a number,\nand you have to find out what that number is");
-    printf("by guessing it again and again.\n It will tell the status of you number accordingly(like your number is greater or smaller than the computer's number\n)");
+    printf("by guessing it again and again.\n It will tell the status of you number accordingly(like your number is greater or smaller than the computer's number)\n");
     int play_again;
 
     do

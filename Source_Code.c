920d2e98d56b2450fd49ba28f4f39d1f797e511c
Mehul Kumar <mehulsubhash888@gmail.com>
2026-05-17 14:19:37 +0530

    Enhance guessing game with welcome message
    Updated the guessing game to include a welcome message and improved instructions.

--- Source_Code.c
+++ Source_Code.c
@@ -7,9 +7,12 @@
 
 int main()
 {
+    printf("Welcome\nIn this game, the computer will think of a number,\nand you have to find out what that number is");
+    printf("by guessing it again and again.\n It will tell the status of you number accordingly(like your number is greater or smaller than the computer's number\n)");
     int play_again;
 
-    do{
+    do
+    {
         srand(time(0));
         int RandomNumber = (rand() % 100) + 1;
         int x;
@@ -19,22 +22,26 @@ int main()
         {
             printf("Guess The Number:");
             scanf("%d", &x);
-            if (x < RandomNumber){
+            if (x < RandomNumber)
+            {
                 printf("Higher Number Please\n");
             }
-            else if (x > RandomNumber){
+            else if (x > RandomNumber)
+            {
                 printf("Lower Number Please\n+");
             }
-            else{
+            else
+            {
                 printf("Congrats");
             }
             count++;
         } while (x != RandomNumber);
-    printf("Great You Guessed It Right\n The Number Was %d", RandomNumber);
-    printf("\nYou Figured it in attempt %d", count + 1);
-    printf("\nWould you like to try again, submit 1 for yess and 0 to quite:");
-    scanf("%d",&play_again);
-    }while(play_again == 1) ;
+        printf("Great You Guessed It Right\n The Number Was %d", RandomNumber);
+        printf("\nYou Figured it in attempt %d", count + 1);
+        printf("\nWould you like to try again, submit 1 for yess and 0 to quite:");
+        scanf("%d", &play_again);
+    } while (play_again == 1);
 
     return 0;
+}
 }
